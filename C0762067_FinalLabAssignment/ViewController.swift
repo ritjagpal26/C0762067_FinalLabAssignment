965de7b3554af115d4ebb37ae9d11566287b1c97
Ritik Jagpal <55303144+ritjagpal26@users.noreply.github.com>
2020-01-24 18:06:26 -0500

    Initial Commit

---
+++ C0762067_FinalLabAssignment/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  C0762067_FinalLabAssignment
+//
+//  Created by Mac on 2020-01-24.
+//  Copyright © 2020 Mac. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
